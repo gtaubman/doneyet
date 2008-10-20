0acbe771f73a34e0d1bd9145f869db5ec5755fcf
gtaubman <gtaubman@e1507820-b22f-0410-a5aa-175571c60047>
2008-10-20 04:24:21 +0000

    Support compilation on OpenBSD, thanks to Rivo Nurges.

--- serializer.cc
+++ serializer.cc
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include "serializer.h"
 #include <iostream>
 #include <fstream>
--- utils.cc
+++ utils.cc
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <string.h>
 #include "utils.h"
 #include <stdio.h>
