3f9e7c0d0219517a5c06e9077fc15342a62b93e6
gtaubman <gtaubman@e1507820-b22f-0410-a5aa-175571c60047>
2008-10-27 03:46:48 +0000

    Fix a segfault when attempting to delete a task when none was selected. Only show the notes view if the window is large enough. Fix arrow key bindings.
    Credit to Rivo Nurges for finding the bugs.

---
+++ constants.h
@@ -0,0 +1,13 @@
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
+class Constants {
+ public:
+  // The minimum width of the window in order to show a separate notes view.
+  static const int kMinWidthForNotesView = 120;
+
+  // How large to draw the notes view.
+  static const int kNoteViewSize = 40;
+};
+
+#endif
