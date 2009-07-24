c9192254e4cfe668a7de641ba92a978858ada47a
gtaubman <gtaubman@e1507820-b22f-0410-a5aa-175571c60047>
2009-07-24 04:07:34 +0000

    - Parse the task color related configuration options. - Add and parse configuration options for menu colors. - Update the wiki page documenting all the configuration options.

--- trunk/doneyet-config.h
+++ trunk/doneyet-config.h
@@ -4,6 +4,9 @@
 #include <map>
 #include <string>
 
+using std::map;
+using std::string;
+
 class DoneyetConfig {
  public:
   bool Parse();
@@ -13,11 +16,29 @@ class DoneyetConfig {
   short BackgroundColor() { return background_color_; }
 
  private:
-  std::map<std::string, std::map<std::string, std::string> > config_;
+  map<string, map<string, string> > config_;
 
-  bool ParseColors();
+  // Attempts to convert config[color_name] into a color, and set var_to_set to
+  // that color.  If this fails it prints an error and return false.
+  bool ParseColor(map<string, string>& config,
+                  const string& color_name,
+                  short* var_to_set);
+
+  bool ParseGeneralOptions();
   short foreground_color_;
   short background_color_;
+
+  bool ParseTaskOptions();
+  short unstarted_task_color_;
+  short in_progress_task_color_;
+  short paused_task_color_;
+  short finished_task_color_;
+
+  bool ParseMenuOptions();
+  short unselected_menu_foreground_color_;
+  short unselected_menu_background_color_;
+  short selected_menu_foreground_color_;
+  short selected_menu_background_color_;
 };
 
 #endif
