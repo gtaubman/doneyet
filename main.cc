a4417a9ce5dcf4a0b6231d3ba70f51685ba12df6
Steffen Weise <steffen.weise@mercateo.com>
2019-01-04 20:48:41 +0100

    automated formatting for readability

--- main.cc
+++ main.cc
@@ -10,64 +10,64 @@ using std::ofstream;
 using std::iostream;
 
 int main(int argc, char** argv) {
-  if(argc>=2)
-  {
+    if(argc>=2) // at least one argument or more
+    {
         printf("%s does not understand any command line arguments yet.\nHere, have a help page :-)\n",argv[0]);
-	printf("%s", __HELPTEXT__);
-	return 0;
-  }
-  else
-  {
-  bool curses = true;
-
-  DoneyetConfig* config = DoneyetConfig::GlobalConfig();
-  if (config == NULL) {
-    fprintf(stderr, "Unable to parse config file.\n");
-    return 1;
-  }
-
-  if (curses) {
-    setlocale(LC_ALL, "");
-    initscr();  // Create the standard window.
-    keypad(stdscr, true);        // Enable keyboard mappings
-    nonl();                         // Disable weird newline stuff.
-    cbreak();                       // Take input characters one at a time.
-    noecho();                       // We don't want input to be echoed
-    intrflush(stdscr, FALSE);
-
-
-    // If our terminal has color, start up colors
-    if (has_colors()) {
-      start_color();
-
-      if (assume_default_colors(config->ForegroundColor(),
-                                config->BackgroundColor()) != OK) {
-        fprintf(stderr, "Unable to set default colors.\n");
-        endwin();
-        return 1;
-      }
-
-      // Make some color pairs
-      // TODO: Make the task drawing know which color pairs to use.
-      short background_color = config->BackgroundColor();
-      init_pair(1, COLOR_RED,     background_color);
-      init_pair(2, COLOR_GREEN,   background_color);
-      init_pair(3, COLOR_YELLOW,  background_color);
-      init_pair(4, COLOR_BLUE,    background_color);
-      init_pair(5, COLOR_CYAN,    background_color);
-      init_pair(6, COLOR_MAGENTA, background_color);
-      init_pair(7, COLOR_WHITE,   background_color);
-      init_pair(8, COLOR_YELLOW,   COLOR_BLUE);
-      init_pair(9, COLOR_YELLOW,   COLOR_BLUE);
+        printf("%s", __HELPTEXT__);
+        return 0;
     }
-  }
+    else // no arguments supplied
+    {
+        bool curses = true;
 
-  if (curses) {
-    Workspace w;
-    endwin();
-  }
+        DoneyetConfig* config = DoneyetConfig::GlobalConfig();
+        if (config == NULL) {
+            fprintf(stderr, "Unable to parse config file.\n");
+            return 1;
+        }
 
-  delete config;
-  return 0;
-  }
+        if (curses) {
+            setlocale(LC_ALL, "");
+            initscr();  // Create the standard window.
+            keypad(stdscr, true);        // Enable keyboard mappings
+            nonl();                         // Disable weird newline stuff.
+            cbreak();                       // Take input characters one at a time.
+            noecho();                       // We don't want input to be echoed
+            intrflush(stdscr, FALSE);
+
+
+            // If our terminal has color, start up colors
+            if (has_colors()) {
+                start_color();
+
+                if (assume_default_colors(config->ForegroundColor(),
+                                          config->BackgroundColor()) != OK) {
+                    fprintf(stderr, "Unable to set default colors.\n");
+                    endwin();
+                    return 1;
+                }
+
+                // Make some color pairs
+                // TODO: Make the task drawing know which color pairs to use.
+                short background_color = config->BackgroundColor();
+                init_pair(1, COLOR_RED,     background_color);
+                init_pair(2, COLOR_GREEN,   background_color);
+                init_pair(3, COLOR_YELLOW,  background_color);
+                init_pair(4, COLOR_BLUE,    background_color);
+                init_pair(5, COLOR_CYAN,    background_color);
+                init_pair(6, COLOR_MAGENTA, background_color);
+                init_pair(7, COLOR_WHITE,   background_color);
+                init_pair(8, COLOR_YELLOW,   COLOR_BLUE);
+                init_pair(9, COLOR_YELLOW,   COLOR_BLUE);
+            }
+        }
+
+        if (curses) {
+            Workspace w;
+            endwin();
+        }
+
+        delete config;
+        return 0;
+    }
 }
--- workspace.h
+++ workspace.h
@@ -41,101 +41,114 @@ using std::string;
 "* q - Quit.\n"
 
 class StringListItem : public ListItem {
- public:
-  StringListItem(string str) : ListItem() {
-    str_ = str;
-  }
+public:
+    StringListItem(string str) : ListItem() {
+        str_ = str;
+    }
 
-  // Required Functions
-  virtual const string TextForColumn(const string& c) { return str_; }
-  virtual int NumListChildren() { return 0; }
-  virtual ListItem* ListChild(int i) { assert(false); return NULL; }
-  virtual ListItem* ListParent() { return NULL; }
-  virtual void SetListText(string& text) { }
-  
- private:
-  string str_;
+    // Required Functions
+    virtual const string TextForColumn(const string& c) {
+        return str_;
+    }
+    virtual int NumListChildren() {
+        return 0;
+    }
+    virtual ListItem* ListChild(int i) {
+        assert(false);
+        return NULL;
+    }
+    virtual ListItem* ListParent() {
+        return NULL;
+    }
+    virtual void SetListText(string& text) { }
+
+private:
+    string str_;
 };
 
 class StringVectorSource : public HierarchicalListDataSource {
- public:
-  StringVectorSource() { }
-  virtual ~StringVectorSource() { }
+public:
+    StringVectorSource() { }
+    virtual ~StringVectorSource() { }
 
-  void SetVector(const vector<string> strings) {
-    for (int i = 0; i < list_items_.size(); ++i) {
-      delete list_items_[i];
+    void SetVector(const vector<string> strings) {
+        for (int i = 0; i < list_items_.size(); ++i) {
+            delete list_items_[i];
+        }
+        list_items_.clear();
+
+        for (int i = 0; i < strings.size(); ++i) {
+            list_items_.push_back(new StringListItem(strings[i]));
+        }
     }
-    list_items_.clear();
-
-    for (int i = 0; i < strings.size(); ++i) {
-      list_items_.push_back(new StringListItem(strings[i]));
+    void Clear() {
+        for (int i = 0; i < list_items_.size(); ++i) {
+            delete list_items_[i];
+        }
+        list_items_.clear();
     }
-  }
-  void Clear() {
-    for (int i = 0; i < list_items_.size(); ++i) {
-      delete list_items_[i];
+
+    // Required functions
+    virtual int NumRoots() {
+        return list_items_.size();
     }
-    list_items_.clear();
-  }
+    virtual ListItem* Root(int i) {
+        return list_items_[i];
+    }
 
-  // Required functions
-  virtual int NumRoots() { return list_items_.size(); }
-  virtual ListItem* Root(int i) { return list_items_[i]; }
-
- private:
-  vector<StringListItem*> list_items_;
+private:
+    vector<StringListItem*> list_items_;
 };
 
 class Workspace {
- public:
-  explicit Workspace();
-  virtual ~Workspace();
+public:
+    explicit Workspace();
+    virtual ~Workspace();
 
-  void Run();
-  void Quit();
+    void Run();
+    void Quit();
 
- private:
-  void NewProject();
-  void OpenProject();
-  void SaveCurrentProject();
+private:
+    void NewProject();
+    void OpenProject();
+    void SaveCurrentProject();
 
-  void InitializeLists();
+    void InitializeLists();
 
-  // UI Actions
-  void AddTask(Task* t);
-  void MoveTask(Task* t);
-  void ShowMenuBar(Task* t);
-  void AddNote(Task* t);
-  void ViewNotes(Task* t);
-  void ToggleStatus(Task* t);
+    // UI Actions
+    void AddTask(Task* t);
+    void MoveTask(Task* t);
+    void ShowMenuBar(Task* t);
+    void AddNote(Task* t);
+    void ViewNotes(Task* t);
+    void ToggleStatus(Task* t);
 
-  // UI Helper Functions
-  void HandleMenuInput(const string& input);
-  void RunFind();
-  void PerformFullListUpdate();
-  void DisplayNotes(Task* t);
-  void DisplayHelp();
+    // UI Helper Functions
+    void HandleMenuInput(const string& input);
+    void RunFind();
+    void PerformFullListUpdate();
+    void DisplayNotes(Task* t);
+    void DisplayHelp();
 
-  // Initialization Helper Functions
-  void InitializeMenuBar();
-  Project* CreateNewProject();
+    // Initialization Helper Functions
+    void InitializeMenuBar();
+    Project* CreateNewProject();
 
-  // Filter Helpers
-  void ShowAllTasks();
-  void ShowTasksCompletedLastWeek();
-  void ShowUnfinishedTasks();
+    // Filter Helpers
+    void ShowAllTasks();
+    void ShowTasksCompletedLastWeek();
+    void ShowUnfinishedTasks();
 
-  static void SigWinchHandler(int signal,
-                              siginfo_t* siginfo,
-                              void* signal_ucontext);
-  
-  MenuBar* menubar_;
-  Project* project_;
-  HierarchicalList* list_;
-  HierarchicalList* notes_list_;
-  StringVectorSource notes_source_;
-  bool done_;
+    static void SigWinchHandler(int signal,
+                                siginfo_t* siginfo,
+                                void* signal_ucontext);
+
+    MenuBar* menubar_;
+    Project* project_;
+    HierarchicalList* list_;
+    HierarchicalList* notes_list_;
+    StringVectorSource notes_source_;
+    bool done_;
 };
 
 #endif
