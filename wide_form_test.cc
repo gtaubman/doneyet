4ce89f027030e9c2bd85b512310857bfb28b9b5a
Steffen Weise <stweise@posteo.de>
2024-01-28 22:17:20 +0100

    more testing

--- wide_form_test.cc
+++ wide_form_test.cc
@@ -3,19 +3,11 @@
 #include <ncursesw/form.h>
 #include <clocale>
 
-#undef CTRL
-#define CTRL(x)	((x) & 0x1f)
-#define ESCAPE		CTRL('[')
-
 int main() {
 
-#define NUM_FIELDS 3
-#define MyRow(n) (4 + (n) * 2)
-#define MyCol(n) 10
-    FIELD *field[NUM_FIELDS + 1];
+    FIELD *fields[2];
     FORM *my_form;
     bool done = FALSE;
-    int n;
 
     setlocale(LC_ALL, "");
 
@@ -24,34 +16,23 @@ int main() {
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
-
-    /* Initialize the fields */
-    for (n = 0; n < NUM_FIELDS; ++n) {
-        field[n] = new_field(1, 10, MyRow(n), 18, 0, 0);
-        set_field_back(field[n], A_UNDERLINE);
-        /* Print a line for the option  */
-        field_opts_off(field[n], O_AUTOSKIP);
-        /* Don't go to next field when this is filled */
-    }
-    field[n] = nullptr;
+    fields[0] = new_field(1, 10, 0, 18, 0, 0);
+    set_field_back(fields[0], A_UNDERLINE);
+    fields[1] = nullptr;
 
     /* Create the form and post it */
-    my_form = new_form(field);
+    my_form = new_form(fields);
     post_form(my_form);
     refresh();
-
-    for (n = 0; n < NUM_FIELDS; ++n) {
-        mvprintw(MyRow(n), MyCol(n), "Value %d:", n + 1);
-    }
+    mvprintw(0, 0, "Value: ");
 
     /* Loop through to get user requests */
     while (!done) {
         wint_t c2;
         int ret = get_wch(&c2);
+        int localrc=0;
 
-        mvprintw(MyRow(NUM_FIELDS),
-                 MyCol(NUM_FIELDS),
-                 "Got %d (%#x), type: %s",
+        mvprintw(2, 2, "Got %d (%#x), type: %s",
                  (int) c2,
                  (int) c2,
                  (ret == KEY_CODE_YES)
@@ -61,51 +42,39 @@ int main() {
                     : ((ret == ERR)
                        ? "ERR"
                        : "?")));
+        mvprintw(3, 2, "Memory: %p", field_buffer(fields[0], 0));
+        mvprintw(4, 2, "RC: %d", localrc);
         clrtoeol();
 
-        switch (ret) {
-            case KEY_CODE_YES: //we have a key code, so deal with that (not a wide char)
-                switch (c2) {
-                    case KEY_DOWN:
-                        /* Go to next field */
-                        form_driver_w(my_form, KEY_CODE_YES, REQ_NEXT_FIELD);
-                        /* Go to the end of the present buffer */
-                        /* Leaves nicely at the last character */
-                        form_driver_w(my_form, KEY_CODE_YES, REQ_END_LINE);
-                        break;
-                    case KEY_BTAB:
-                    case KEY_UP:
-                        /* Go to previous field */
-                        form_driver_w(my_form, KEY_CODE_YES, REQ_PREV_FIELD);
-                        form_driver_w(my_form, KEY_CODE_YES, REQ_END_LINE);
-                        break;
-                    default:
-                        break;
-                }
-                break;
-            case OK: // we have a wide char, deal with that
-                switch (c2) {
-                    case ESCAPE:
-                        done = TRUE;
-                        break;
-                    case '\t':
-                        form_driver_w(my_form, KEY_CODE_YES, REQ_NEXT_FIELD);
-                        form_driver_w(my_form, KEY_CODE_YES, REQ_END_LINE);
-                        break;
-                    default:
-                        form_driver_w(my_form, OK, (wchar_t) c2);
-                        break;
-                }
-                break;
+        if (ret == KEY_CODE_YES) { //we have a key code, so deal with that (not a wide char)
+            if (c2 ==  KEY_DOWN){
+                //localrc = form_driver_w(my_form, KEY_CODE_YES, REQ_END_LINE);
+                localrc = form_driver_w(my_form, KEY_CODE_YES, REQ_VALIDATION);
+                done = TRUE;
+            }
+        } else if (ret == OK) { //this is a wide char
+            localrc = form_driver_w(my_form, OK, (wchar_t) c2);
         }
     }
 
+    mvprintw(9,0, "Hello");
+    int rc = form_driver_w(my_form, KEY_CODE_YES, REQ_VALIDATION);
+    if (rc != E_OK) {
+        mvprintw(10, 0, "Validation failed, got: %d", rc);
+    }
+    //wchar_t buffer[100];
+    //memcpy(buffer, field_buffer(fields[0], 0), 100 );
+    wchar_t * ptr = (wchar_t *) field_buffer(fields[0], 0);
+    mvprintw(11,0, "Pointer: %p", ptr);
+    mvprintw(12,0, "Printing test: %ls", L"testing baby");
+    mvprintw(13,0, "Pointer Content: %ls", ptr );
+    mvprintw(14,0, "Printing test: %ls", L"testing baby");
+
+    getch();
     /* Un post form and free the memory */
     unpost_form(my_form);
     free_form(my_form);
-    for (n = 0; n < NUM_FIELDS; ++n) {
-        free_field(field[n]);
-    }
+    free_field(fields[0]);
     endwin();
     return 0;
 }
